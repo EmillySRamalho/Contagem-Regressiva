7476eb379c9c40be2222f915359972f8f3658d37
EmillySRamalho <emilly_bsfpb@hotmail.com>
2026-02-03 16:34:16 -0300

    contagem regressiva

---
+++ script.js
@@ -0,0 +1,23 @@
+import Countdown from "./countdown.js";
+
+const diasParaOMeuAniversario = new Countdown(
+  '24 March 2026 GMT-0300'
+);
+
+const daysEl = document.getElementById("days");
+const hoursEl = document.getElementById("hours");
+const minutesEl = document.getElementById("minutes");
+const secondsEl = document.getElementById("seconds");
+
+function updateCountdown() {
+  const { days, hours, minutes, seconds } =
+    diasParaOMeuAniversario.total;
+
+  daysEl.textContent = String(days).padStart(2, "0");
+  hoursEl.textContent = String(hours).padStart(2, "0");
+  minutesEl.textContent = String(minutes).padStart(2, "0");
+  secondsEl.textContent = String(seconds).padStart(2, "0");
+}
+
+updateCountdown();
+setInterval(updateCountdown, 1000);
